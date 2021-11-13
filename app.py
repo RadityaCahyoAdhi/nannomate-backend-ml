75c1fbc64e7c689e540c8a28b794e42f5635981c
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2021-11-13 15:53:05 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+# import library
+from flask import Flask, request
+from flask_restful import Resource, Api
+# from flask_cors import CORS
+
+# Inisiasi object flask
+app = Flask(__name__)
+
+# inisiasi object flask_restful
+api = Api(app)
+
+# inisiasi variabel kosong bertipe dictionary
+fitur = {} # variable global , dictionary = json
+
+import pandas as pd
+# from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+ 
+# Membaca file iris.csv
+iris = pd.read_csv('Iris.csv')
+
+# menghilangkan kolom yang tidak penting
+iris.drop('Id',axis=1,inplace=True)
+
+# memisahkan atribut dan label
+X = iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm' ]]
+y = iris['Species']
+ 
+# membuat model Decision Tree
+tree_model = DecisionTreeClassifier() 
+ 
+# melakukan pelatihan model terhadap data
+tree_model.fit(X, y)
+
+# membuat class Resource
+class MachineLearningResource(Resource):
+    # metode post
+    def post(self):
+        fitur["sepal_length"] = request.form["sepal_length"]
+        fitur["sepal_width"] = request.form["sepal_length"]
+        fitur["petal_length"] = request.form["petal_length"]
+        fitur["petal_width"] = request.form["petal_width"]
+
+        # prediksi model dengan tree_model.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
+        prediction = tree_model.predict([[fitur["sepal_length"], fitur["sepal_width"], fitur["petal_length"], fitur["petal_width"]]])
+        response = {"prediction" : prediction[0]}
+        return response
+
+# setup resourcenya
+api.add_resource(MachineLearningResource, "/api/predict", methods=["POST"])
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005)
