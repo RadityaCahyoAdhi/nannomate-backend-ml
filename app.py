5d24fb77d0123ac0d91969fcbee3919caff8cf99
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2021-11-15 13:17:34 +0700

    updated app.py (copy paste from dev branch) and deleted debug code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # import library
 from flask import Flask, request
 from flask_restful import Resource, Api
+from jcopml.utils import load_model
+import numpy as np
 
 # Inisiasi object flask
 app = Flask(__name__)
@@ -8,42 +10,50 @@ app = Flask(__name__)
 # inisiasi object flask_restful
 api = Api(app)
 
+# load label encoder
+lbl_encoder = load_model("model/lbl_encoder_nannomate.pkl")
+
+# load machine learning model 
+random_forest_model = load_model("model/random_forest_nannomate.pkl")
+
 # inisiasi variabel kosong bertipe dictionary
 fitur = {} # variable global , dictionary = json
-
-import pandas as pd
-# from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
- 
-# Membaca file iris.csv
-iris = pd.read_csv('Iris.csv')
-
-# menghilangkan kolom yang tidak penting
-iris.drop('Id',axis=1,inplace=True)
-
-# memisahkan atribut dan label
-X = iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm' ]]
-y = iris['Species']
- 
-# membuat model Decision Tree
-tree_model = DecisionTreeClassifier() 
- 
-# melakukan pelatihan model terhadap data
-tree_model.fit(X, y)
 
 # membuat class Resource
 class MachineLearningResource(Resource):
     # metode post
     def post(self):
-        fitur["sepal_length"] = request.form["sepal_length"]
-        fitur["sepal_width"] = request.form["sepal_length"]
-        fitur["petal_length"] = request.form["petal_length"]
-        fitur["petal_width"] = request.form["petal_width"]
+        fitur["jumlah_lengan"] = request.form["jumlah_lengan"]
+        fitur["bercabang"] = request.form["bercabang"]
+        fitur["knob"] = request.form["knob"]
+        fitur["bentuk_lengan"] = request.form["bentuk_lengan"]
+        fitur["ujung_lengan"] = request.form["ujung_lengan"]
 
-        # prediksi model dengan tree_model.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
-        prediction = tree_model.predict([[fitur["sepal_length"], fitur["sepal_width"], fitur["petal_length"], fitur["petal_width"]]])
-        response = {"prediction" : prediction[0]}
-        return response
+        # prediksi probabilitas masing-masing kelas
+        probs = random_forest_model.predict_proba([[fitur["jumlah_lengan"],fitur["bercabang"],fitur["knob"],fitur["bentuk_lengan"],fitur["ujung_lengan"]]])
 
-# setup resourcenya
+        # mengurutkan probabilitas dari yang terbesar ke yang terkecil
+        sorted_probs = sorted(probs[0], reverse=True)
+
+        # mencari 3 kelas spesies dengan probabilitas terbesar
+        best_three_species = np.argsort(probs)[:,-3:][0]
+        
+        # melakukan inverse transform pada 3 kelas spesies dengan probabilitas terbesar
+        decoded_best_three_species = lbl_encoder.inverse_transform(best_three_species)
+
+        response = {
+                        "prediction": {
+                            "first_prediction": decoded_best_three_species[2],
+                            "second_prediction": decoded_best_three_species[1],
+                            "third_prediction": decoded_best_three_species[0]
+                        },
+                        "probabilities": {
+                            "first_probability": round(sorted_probs[0]*100, 2),
+                            "second_prediction": round(sorted_probs[1]*100, 2), 
+                            "third_prediction": round(sorted_probs[2]*100, 2)
+                        }
+                    }
+        return response, 200
+
+# setup resource
 api.add_resource(MachineLearningResource, "/api/predict", methods=["POST"])
